e62889244df5c366c051e3b6eaecf253bb7bf3d9
salwa mazarin <tech_dev4@indicocompany.com>
2024-06-12 19:05:01 +0700

    Feat: header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// import { Inter, Nunito } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,9 +14,52 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const menu = [
+    {
+      title: "Home",
+      link: "/",
+      number: "01",
+    },
+    {
+      title: "Expertise",
+      link: "#expertise",
+      number: "02",
+    },
+    {
+      title: "Work",
+      link: "#work",
+      number: "03",
+    },
+    {
+      title: "Experience",
+      link: "#experience",
+      number: "04",
+    },
+    {
+      title: "Contact",
+      link: "$contact",
+      number: "05",
+    },
+  ];
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <div className="flex gap-12 justify-center">
+          {menu.map((x) => {
+            return (
+              <div className="relative">
+                <div className="menu-item">
+                  <div className="menu-text">
+                    <p>{x.number}</p>
+                    <p>{x.title}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
